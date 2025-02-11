0986172412de1f6e517d3e99cf8c0dbca7a88d91
Gustavo Henrique <guazambujinha@gmail.com>
2025-02-11 15:47:36 -0300

    fist commit

---
+++ Baixador-Video/src/main/java/Baixador_Video/BaixadorVideoApplication.java
@@ -0,0 +1,13 @@
+package Baixador_Video;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaixadorVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaixadorVideoApplication.class, args);
+	}
+
+}
---
+++ Baixador-Video/src/test/java/Baixador_Video/BaixadorVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package Baixador_Video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaixadorVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
